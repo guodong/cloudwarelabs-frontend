217b4e80efcd2f99623d6708b58da7b08d0bc670
zhangliqing <623009470@qq.com>
2017-06-05 22:42:19 +0800

    add welcome page

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Cloudwares from '@/components/cloudwares/Index.vue'
 import Instances from '@/components/instances/Index.vue'
 import InstancesView from '@/components/instances/instance/View.vue'
 import Instance from '@/components/instances/instance/Index.vue'
+import Welcome from '@/components/Welcome.vue'
+import Login from '@/components/Login.vue'
+import Register from '@/components/Register.vue'
 
 Vue.use(Router)
 
@@ -11,19 +14,27 @@ export default new Router({
   mode: 'history',
   linkActiveClass: 'active',
   routes: [{
-      path: '/cloudwares',
-      component: Cloudwares
-    }, {
-      path: '/instances',
-      component: Instances,
-    }, {
-      path: '/instances/:id',
-      component: Instance,
-      children: [{
-        path: 'view',
-        name: 'instancesView',
-        component: InstancesView
-      }]
-    }
-  ]
+    path: '/cloudwares',
+    component: Cloudwares
+  }, {
+    path: '/instances',
+    component: Instances,
+  }, {
+    path: '/instances/:id',
+    component: Instance,
+    children: [{
+      path: 'view',
+      name: 'instancesView',
+      component: InstancesView
+    }]
+  }, {
+    path:'/',
+    component: Welcome
+  }, {
+    path: '/login',
+    component: Login
+  }, {
+    path: '/register',
+    component: Register
+  }]
 })
